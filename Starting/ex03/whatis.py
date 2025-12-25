1fe5bab8c46e867d2991ff35dad17f6fff4d8498
barrak7 <hamzabarrak83@gmail.com>
2025-12-25 09:23:33 +0100

    fix: proper handling of negative values

--- Starting/ex03/whatis.py
+++ Starting/ex03/whatis.py
@@ -2,10 +2,12 @@ import sys
 
 
 def whatis(args: list[str]) -> None:
-    assert len(args) == 2, "AssertionError: more than one argument is provided"
-    assert args[1].isnumeric() == True, "AssertionError: argument is not an integer"
+    assert len(args) == 2, "more than one argument is provided"
 
-    num: int = int(args[1])
+    try:
+        num: int = int(args[1])
+    except ValueError:
+        raise AssertionError("argument is not an integer")
 
     print(("I'm Even.", "I'm Odd.")[num % 2])
 
@@ -15,4 +17,4 @@ if __name__ == "__main__":
     try:
         whatis(sys.argv)
     except AssertionError as e:
-        print(e)
+        print("AssertionError:",e)
