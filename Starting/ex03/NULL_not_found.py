f97dd8e13f5c6e7847d128b24309f20e62048e5e
barrak7 <hamzabarrak83@gmail.com>
2025-12-25 10:18:17 +0100

    Starting(ex03): check if value of object is null and print its type.

---
+++ Starting/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def NULL_not_found(object: any) -> int:
+    if not object or object != object:
+        val: str = str(object)
+        types: dict[str, str] = {
+            "": "Empty:",
+            "None": "Nothing: None",
+            "nan": "Cheese: nan",
+            "0": "Zero: 0",
+            "False": "Fake: False"
+        }
+
+        if val in types:
+            print(types[val], type(object))
+            return 0
+
+    print("Type not Found")
+    return 1
